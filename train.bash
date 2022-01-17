80b9ea0ea73112c7ede4ea6ab5c1dce21f58fb74
Simon Hanna <simon.hanna@aeb.com>
2022-01-17 10:42:08 +0100

    Implement patching

--- train.bash
+++ train.bash
@@ -6,28 +6,27 @@ cd "$(git rev-parse --show-toplevel)"
 
 [ -n "$docker_username" ]
 [ -n "$docker_password" ]
-[ -n "$docker_repo" ]
 
 docker login --username "$docker_username" --password "$docker_password"
 
 tag="$(cat .latest_version)"
 
-curl "https://hub.docker.com/v2/repositories/${docker_username}/${docker_repo}/tags" -o /tmp/docker_tags.json
+# curl "https://hub.docker.com/v2/repositories/${docker_username}/${docker_repo}/tags" -o /tmp/docker_tags.json
 
-if jq -r '.results[] | .name' /tmp/docker_tags.json | grep -E "^${tag}$"; then
-  echo "======================================
-=  Tag '$tag' already in docker hub  =
-======================================"
-  exit 0
-fi
+# if jq -r '.results[] | .name' /tmp/docker_tags.json | grep -E "^${tag}$"; then
+#   echo "======================================
+# =  Tag '$tag' already in docker hub  =
+# ======================================"
+#   exit 0
+# fi
 
-export IMAGE="$docker_username/$docker_repo"
-export VERSION="$tag-oss"
+export IMAGE="simhnna/sourcegraph"
+export VERSION="$tag"
 
 cd sourcegraph
 
-git fetch origin refs/tags/$tag:refs/tags/$tag
-git checkout "tags/$tag" -b "$tag-release-branch-$docker_username"
+git fetch origin refs/tags/v$tag:refs/tags/v$tag
+git checkout "tags/v$tag" -b "$tag-release-branch-simhnna"
 
 if [ 'Linux' == "$(uname -s)" ]; then
   sudo apt-get install musl-tools
@@ -37,10 +36,9 @@ cd cmd/server
 ./pre-build.sh
 ./build.sh
 
-docker tag "${docker_username}/${docker_repo}:latest" "${docker_username}/${docker_repo}:${tag}"
-docker tag "${docker_username}/${docker_repo}:latest" "${docker_username}/${docker_repo}:${VERSION}"
+docker tag "${IMAGE}:latest" "${IMAGE}:${tag}"
 
-docker push --all-tags "${docker_username}/${docker_repo}"
+docker push "${IMAGE}:${tag}"
 
 echo '===========
 =  Done!  =
